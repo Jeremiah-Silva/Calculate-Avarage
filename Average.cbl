67dcbb52aaacfe0a655c2cd720e0f4564c786138
Jeremiah Silva <jeremiah.ca9@gmail.com>
2023-04-21 23:54:13 -0600

    Updated File

--- Average.cbl
+++ Average.cbl
@@ -11,29 +11,31 @@
 
        DATA DIVISION.
        WORKING-STORAGE SECTION.
-       01 STUDENT-NAME PIC X(50).
-       01 SUBJECT PIC X(50).
-       01 SCORE1 PIC 9(1)V9(2).
-       01 SCORE2 PIC 9(1)V9(2).
-       01 SCORE3 PIC 9(1)V9(2).
-       01 SCORE4 PIC 9(1)V9(2).
-       01 AVERAGE PIC 9(1)V9(2).
-       01 OPTION PIC X.
-       01 RESULT PIC X(10).
+       01 STUDENT-NAME           PIC X(50).
+       01 SUBJECT                PIC X(50).
+       01 SCORE1                 PIC 9(1)V9(2).
+       01 SCORE2                 PIC 9(1)V9(2).
+       01 SCORE3                 PIC 9(1)V9(2).
+       01 SCORE4                 PIC 9(1)V9(2).
+       01 AVERAGE                PIC 9(1)V9(2).
+       01 OPTION                 PIC X.
+       01 RESULT                 PIC X(10).
 
        PROCEDURE DIVISION.
        WELCOME.
            DISPLAY "     ".
+           DISPLAY "**************************************************".
            DISPLAY "WELCOME TO JEREMIAH COLLEGE'S APP.".
+           DISPLAY "**************************************************".
            DISPLAY "     ".
        SNAME.
            DISPLAY "ENTER THE STUDENT'S NAME:".
-           ACCEPT STUDENT-NAME.
+           ACCEPT               STUDENT-NAME.
            DISPLAY "     ".
 
        SUBJECT-NAME.
            DISPLAY "ENTER THE COURSE SUBJECT:".
-           ACCEPT SUBJECT.
+           ACCEPT               SUBJECT.
            DISPLAY "     ".
            
            PERFORM CALCULATE-AVERAGE
@@ -43,28 +45,28 @@
 
        CALCULATE-AVERAGE.
            DISPLAY "ENTER THE FIRST GRADE: ".
-           ACCEPT SCORE1.
+           ACCEPT                SCORE1.
            DISPLAY "     "
            IF SCORE1 <= 0
            DISPLAY "INVALID INPUT FOR THE FIRST GRADE."
            GO TO CALCULATE-AVERAGE.
 
            DISPLAY "ENTER THE SECOND GRADE: ".
-           ACCEPT SCORE2.
+           ACCEPT                SCORE2.
            DISPLAY "     "
            IF SCORE2 <= 0
            DISPLAY "INVALID INPUT FOR THE SECOND GRADE."
            GO TO CALCULATE-AVERAGE.
 
            DISPLAY "ENTER THE THIRD GRADE: ".
-           ACCEPT SCORE3.
+           ACCEPT                SCORE3.
            DISPLAY "     "
            IF SCORE3 <= 0
            DISPLAY "INVALID INPUT FOR THE THIRD GRADE."
            GO TO CALCULATE-AVERAGE.
 
            DISPLAY "ENTER THE FOURTH GRADE: ".
-           ACCEPT SCORE4.
+           ACCEPT                SCORE4.
            DISPLAY "     "
            IF SCORE4 <= 0
            DISPLAY "INVALID INPUT FOR THE FOURTH GRADE."
@@ -73,28 +75,39 @@
            COMPUTE AVERAGE = (SCORE1 + SCORE2 + SCORE3 + SCORE4) / 4.
 
            IF AVERAGE >= 7
-           MOVE "PASSED" TO RESULT
+           MOVE "PASSED"           TO RESULT
            ELSE
-           MOVE "FAILL" TO RESULT
+           MOVE "FAILL"            TO RESULT
            END-IF
-
-           DISPLAY "STUDENT: ", STUDENT-NAME
-           DISPLAY "SUBJECT: ", SUBJECT
-           DISPLAY "AVERAGE: ", AVERAGE
-           DISPLAY "STATUS: ", RESULT.
-
+           DISPLAY "*************** RESULT PROCESSING ****************",
+           DISPLAY "     "
+           DISPLAY "STUDENT NAME    : ",           STUDENT-NAME
+           DISPLAY "SUBJECT         : ",           SUBJECT
+           DISPLAY "AVERAGE         : ",           AVERAGE
+           DISPLAY "STATUS          : ",           RESULT
+           DISPLAY "     "
+           DISPLAY "**************************************************".
+       
        QUESTION.
            DISPLAY "     "
            DISPLAY "WOULD YOU LIKE TO CONTINUE? (Y/N)"
            ACCEPT OPTION.
            IF OPTION = "Y" OR "y"
            DISPLAY "     "
+           DISPLAY "**************************************************",
            DISPLAY "START A NEW SEARCH!"
+           DISPLAY "**************************************************",
            DISPLAY "     "
            PERFORM SUBJECT-NAME
            PERFORM QUESTION
            ELSE IF OPTION = "N" OR "n"
-           DISPLAY "THANK YOU FOR USING THE JEREMIAH COLLEGE'S APP." 
+           DISPLAY "     "
+           DISPLAY "**************************************************",
+           DISPLAY "     "
+           DISPLAY "THANK YOU FOR USING THE JEREMIAH COLLEGE'S APP."
+           DISPLAY "     "
+           DISPLAY "**************************************************",
+           DISPLAY "     "
            ELSE
            DISPLAY "INVALID OPTION. PLEASE ENTER Y OR N."
            PERFORM QUESTION
